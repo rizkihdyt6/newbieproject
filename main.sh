cf4d5ad0efc6db9cba453f315ac27cc80f2bb0d5
M RISQI HIDAYAT <79409216+rizkihdyt6@users.noreply.github.com>
2023-04-06 23:07:47 +0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-### Color
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
