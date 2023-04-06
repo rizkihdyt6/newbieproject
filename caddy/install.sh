2d61bc10bfcedc163ce6af4d51f5560fe1823c6c
M RISQI HIDAYAT <79409216+rizkihdyt6@users.noreply.github.com>
2023-04-06 22:42:54 +0700

    Add files via upload

---
+++ caddy/install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/rizkihdyt6/izinsc/main/ipvps > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f  /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f  /root/tmp
+}
+# https://raw.githubusercontent.com/rizkihdyt6/izinsc/main/ipvps 
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/rizkihdyt6/izinsc/main/ipvps | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/rizkihdyt6/izinsc/main/ipvps | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+
+clear
+REPO="https://raw.githubusercontent.com/rizkihdyt6/newbieproject/main/"
+
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg --yes  >/dev/null 2>&1
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list  >/dev/null 2>&1
+sudo apt install caddy
+
+### Tambah konfigurasi Caddy
+function caddy(){
+    mkdir -p /etc/caddy
+    wget -O /etc/caddy/vmess "${REPO}caddy/vmess" >/dev/null 2>&1
+    wget -O /etc/caddy/vless "${REPO}caddy/vless" >/dev/null 2>&1
+    wget -O /etc/caddy/trojan "${REPO}caddy/trojan" >/dev/null 2>&1
+    wget -O /etc/caddy/ss-ws "${REPO}caddy/ss-ws" >/dev/null 2>&1
+    cat >/etc/caddy/Caddyfile <<-EOF
+$domain:443
+{
+    tls taibabi17@gmail.com
+    encode gzip
+
+    import vless
+    handle_path /vless {
+        reverse_proxy localhost:10001
+
+    }
+
+    import vmess
+    handle_path /vmess {
+        reverse_proxy localhost:10002
+    }
+
+    import trojan
+    handle_path /trojan-ws {
+        reverse_proxy localhost:10003
+    }
+
+    import ss
+    handle_path /ss-ws {
+        reverse_proxy localhost:10004
+    }
+
+    handle {
+        reverse_proxy https://$domain {
+            trusted_proxies 0.0.0.0/0
+            header_up Host {upstream_hostport}
+        }
+    }
+}
+EOF
+}
+
+caddy
+systemctl stop caddy
+systemctl enable --now caddy
